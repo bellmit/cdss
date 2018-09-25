ddc91a1df1dd04f999d53882e6e9401473cebb3b
zhouziyu <1107536493@qq.com>
2018-09-25 11:15:25 +0800

    修复诊疗提醒重复问题

--- cloud-entity/src/main/java/com/jhmk/cloudentity/earlywaring/entity/repository/LogMappingRepository.java
+++ cloud-entity/src/main/java/com/jhmk/cloudentity/earlywaring/entity/repository/LogMappingRepository.java
@@ -2,11 +2,13 @@ package com.jhmk.cloudentity.earlywaring.entity.repository;
 
 import com.jhmk.cloudentity.earlywaring.entity.rule.LogMapping;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
 public interface LogMappingRepository extends PagingAndSortingRepository<LogMapping, Integer>, JpaSpecificationExecutor<LogMapping> {
 
+    @Query("SELECT m from LogMapping m where m.logId=?1 GROUP BY m.logObj,m.logResult,m.logTime,m.logOrderF,m.logOrderS")
     List<LogMapping> findAllByLogId(int logId);
 }
