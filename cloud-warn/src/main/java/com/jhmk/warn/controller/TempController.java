6fc2ed31c656c7bf8437946f8b1371ffc143c6ef
zhouziyu <1107536493@qq.com>
2018-11-05 16:07:34 +0800

    tempController

--- cloud-warn/src/main/java/com/jhmk/warn/controller/TempController.java
+++ cloud-warn/src/main/java/com/jhmk/warn/controller/TempController.java
@@ -5,12 +5,15 @@ import com.jhmk.cloudentity.base.BaseController;
 import com.jhmk.cloudentity.base.BaseEntityController;
 import com.jhmk.cloudentity.earlywaring.entity.UserModel;
 import com.jhmk.cloudentity.earlywaring.entity.rule.Jianchabaogao;
+import com.jhmk.cloudentity.earlywaring.entity.rule.Jianyanbaogao;
 import com.jhmk.cloudentity.earlywaring.entity.rule.Rule;
 import com.jhmk.cloudentity.earlywaring.entity.rule.Yizhu;
 import com.jhmk.cloudservice.warnService.service.RuleService;
 import com.jhmk.cloudutil.model.AtResponse;
 import com.jhmk.cloudutil.model.ResponseCode;
 import com.netflix.discovery.converters.Auto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,15 +24,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * 数据中心接口测试
+ *
  * @author ziyu.zhou
  * @date 2018/9/21 14:36
  */
 @Controller
 @RequestMapping("warn/temp")
 public class TempController extends BaseController {
+    private static final Logger logger = LoggerFactory.getLogger(CmsController.class);
+
     @Autowired
     RuleService ruleService;
 
@@ -53,5 +60,41 @@ public class TempController extends BaseController {
         }
         result.put("yizhu", yizhu);
         wirte(response, result);
+    }
+
+    @PostMapping("/getJianyanbaogaoStr")
+    @ResponseBody
+    public void getJianyanbaogaoStr(HttpServletResponse response, @RequestBody String map) {
+        JSONObject jsonObject = JSONObject.parseObject(map);
+        AtResponse resp = new AtResponse();
+        if (Objects.nonNull(jsonObject)) {
+            String patient_id = jsonObject.getString("patient_id");
+            String visit_id = jsonObject.getString("visit_id");
+            Rule rule = ruleService.getRuleFromDatabase(patient_id, visit_id);
+            //获取 拼接检验检查报告
+            List<Jianyanbaogao> jianchabaogaoStr = ruleService.getJianyanbaogaoStr(rule);
+            resp.setData(jianchabaogaoStr);
+            resp.setResponseCode(ResponseCode.OK);
+            wirte(response, resp);
+        } else {
+            logger.info("医嘱规则匹配传递信息为{}" + map);
+        }
+    }
+
+
+    @PostMapping("/getJianchabaogaoStr")
+    @ResponseBody
+    public void getJianchabaogaoStr(HttpServletResponse response, @RequestBody String map) {
+        JSONObject jsonObject = JSONObject.parseObject(map);
+        if (Objects.nonNull(jsonObject)) {
+            String patient_id = jsonObject.getString("patient_id");
+            String visit_id = jsonObject.getString("visit_id");
+            Rule rule = ruleService.getRuleFromDatabase(patient_id, visit_id);
+            //获取 拼接检验检查报告
+            List<Jianchabaogao> jianchabaogaoStr = ruleService.getJianchabaogaoStr(rule);
+            wirte(response, jianchabaogaoStr);
+        } else {
+            logger.info("医嘱规则匹配传递信息为{}" + map);
+        }
     }
 }
