42c5c79a24f19ab6410c8e64b69ddecbb7ebc1f5
zhouziyu <1107536493@qq.com>
2018-12-10 18:25:23 +0800

    users

--- cloud-entity/src/main/java/com/jhmk/cloudentity/earlywaring/entity/SmUsers.java
+++ cloud-entity/src/main/java/com/jhmk/cloudentity/earlywaring/entity/SmUsers.java
@@ -3,28 +3,37 @@ package com.jhmk.cloudentity.earlywaring.entity;
 import javax.persistence.*;
 import java.util.Objects;
 
+/**
+ * @author ziyu.zhou
+ * @date 2018/11/30 12:08
+ */
+
 @Entity
-@Table(name = "sm_users", schema = "jhmk_waring")
+@Table(name = "sm_users", schema = "jhmk_waring", catalog = "")
 public class SmUsers {
+    private int id;
     private String userId;
     private String userName;
+    private String userLoginName;
+    private String pym;
     private String userDept;
     private String createDate;
     private String userPwd;
     private String roleId;
 
-    @Basic
-    @Column(name = "role_id", nullable = true, length = 255)
-    public String getRoleId() {
-        return roleId;
-    }
 
-    public void setRoleId(String roleId) {
-        this.roleId = roleId;
-    }
-
-    @Basic
     @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
     @Column(name = "USER_ID", nullable = true, length = 255)
     public String getUserId() {
         return userId;
@@ -42,6 +51,26 @@ public class SmUsers {
 
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    @Basic
+    @Column(name = "user_login_name", nullable = true, length = 255)
+    public String getUserLoginName() {
+        return userLoginName;
+    }
+
+    public void setUserLoginName(String userLoginName) {
+        this.userLoginName = userLoginName;
+    }
+
+    @Basic
+    @Column(name = "pym", nullable = true, length = 255)
+    public String getPym() {
+        return pym;
+    }
+
+    public void setPym(String pym) {
+        this.pym = pym;
     }
 
     @Basic
@@ -72,5 +101,37 @@ public class SmUsers {
 
     public void setUserPwd(String userPwd) {
         this.userPwd = userPwd;
+    }
+
+    @Basic
+    @Column(name = "role_id", nullable = true, length = 11)
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SmUsers smUsers = (SmUsers) o;
+        return id == smUsers.id &&
+                Objects.equals(userId, smUsers.userId) &&
+                Objects.equals(userName, smUsers.userName) &&
+                Objects.equals(userLoginName, smUsers.userLoginName) &&
+                Objects.equals(pym, smUsers.pym) &&
+                Objects.equals(userDept, smUsers.userDept) &&
+                Objects.equals(createDate, smUsers.createDate) &&
+                Objects.equals(userPwd, smUsers.userPwd) &&
+                Objects.equals(roleId, smUsers.roleId);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, userId, userName, userLoginName, pym, userDept, createDate, userPwd, roleId);
     }
 }
