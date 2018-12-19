d33e823579ee2ac67cef204248e4e22558d89304
zhouziyu <1107536493@qq.com>
2018-12-19 15:32:25 +0800

    url 地址

---
+++ cloud-util/src/main/java/com/jhmk/cloudutil/config/UrlConstants.java
@@ -0,0 +1,13 @@
+package com.jhmk.cloudutil.config;
+
+/**
+ * 获取鉴别诊断疾病名
+ * @author ziyu.zhou
+ * @date 2018/12/19 14:07
+ */
+
+public class UrlConstants {
+
+    public static final String getDifferentialDiagnosisName = "/med/disease/diosProperty.json";
+
+}
