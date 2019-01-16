9545928ac802b101a531ab7bfd19751cdcc2940e
zhouziyu <1107536493@qq.com>
2019-01-16 17:44:41 +0800

    诊断服务层

---
+++ cloud-page/src/main/java/com/jhmk/cloudpage/service/DiagnoseService.java
@@ -0,0 +1,11 @@
+package com.jhmk.cloudpage.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ziyu.zhou
+ * @date 2019/1/16 17:40
+ */
+@Service
+public class DiagnoseService {
+}
