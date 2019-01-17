71644914b39d52d31d339069f86530b60cb71bcd
zhouziyu <1107536493@qq.com>
2019-01-17 16:05:12 +0800

    用户控制层 添加注释文档

--- cloud-warn/src/main/java/com/jhmk/warn/controller/UsersController.java
+++ cloud-warn/src/main/java/com/jhmk/warn/controller/UsersController.java
@@ -2,22 +2,21 @@ package com.jhmk.warn.controller;
 
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.jhmk.cloudentity.base.BaseEntityController;
 import com.jhmk.cloudentity.earlywaring.entity.SmUsers;
 import com.jhmk.cloudentity.earlywaring.entity.repository.service.SmUsersRepService;
 import com.jhmk.cloudservice.warnService.service.SmUserService;
 import com.jhmk.cloudutil.model.AtResponse;
 import com.jhmk.cloudutil.model.ResponseCode;
+import io.swagger.annotations.*;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.InvocationTargetException;
@@ -39,60 +38,27 @@ public class UsersController extends BaseEntityController<SmUsers> {
     SmUserService smUserService;
 
 
-    @PostMapping("")
-    public String list() {
-        return "/users/list";
-    }
-
-    //分页展示
+    @ApiOperation(value = "分页条件查询", notes = "请求demo:{\"userDept\":\"1020306\"}",
+            httpMethod = "POST", responseContainer = "Map")
+    @ApiResponses({@ApiResponse(code = 200, message = "成功")})
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "map", value = "请求参数", required = true, paramType = "body")
+    })
     @PostMapping(value = "/list")
-    @ResponseBody
     public void userList(HttpServletResponse response, @RequestBody String map) {
         Map<String, Object> reqParams = (Map) JSON.parse(map);
         AtResponse<Map<String, Object>> resp = listData(reqParams, smUsersRepService, "userId");
-        Map<String, Object> data = resp.getData();
-        List<SmUsers> uu = (List<SmUsers>) data.get(LIST_DATA);
-        data.put(LIST_DATA, uu);
-        resp.setResponseCode(ResponseCode.OK);
-        resp.setData(data);
         wirte(response, resp);
     }
 
 
-    //筛选功能
-    @RequestMapping(value = "/serach")
-    @ResponseBody
-    public void serach(HttpServletResponse response, @RequestBody String map) {
-        Map<String, String> fromMap = (Map) JSON.parse(map);
-        String pageNum = fromMap.get("pageNum");
-        SmUsers user = new SmUsers();
-        try {
-            BeanUtils.copyProperties(fromMap, user);
-        } catch (IllegalAccessException e) {
-            logger.error("用户转换失败1：" + fromMap.toString());
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            logger.error("用户转换失败2：" + fromMap.toString());
-            logger.error(e.getMessage());
-            e.printStackTrace();
-        }
-        int page = 1;
-        //当前页不为空
-        if (pageNum != null && !"".equals(pageNum.trim())) {
-            page = new Integer(pageNum) - 1;
-        }
-        Map<String, Object> params = smUserService.serachData(user, page);
-        params.put("user", user);
-        AtResponse<Map<String, Object>> resp = new AtResponse(System.currentTimeMillis());
-        resp.setResponseCode(ResponseCode.OK);
-        resp.setData(params);
-        wirte(response, resp);
-    }
-
-
-    //添加操作
+    @ApiOperation(value = "添加用户", notes = "请求demo:SmUsers",
+            httpMethod = "POST", responseContainer = "Map")
+    @ApiResponses({@ApiResponse(code = 200, message = "成功")})
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "map", value = "请求参数", required = true, paramType = "body")
+    })
     @PostMapping(value = "/add")
-    @ResponseBody
     public void addUser(HttpServletResponse response, @RequestBody SmUsers user) {
         AtResponse<String> resp = new AtResponse<>();
         SmUsers one = smUsersRepService.findOne(user.getUserId());
@@ -113,88 +79,102 @@ public class UsersController extends BaseEntityController<SmUsers> {
         wirte(response, resp);
     }
 
-    //根据id查询用户是否存在
-//    @RequestMapping(value = "/checkUserId")
-//    @ResponseBody
-//    public AtResponse<String> checkUserId(@RequestParam String userId) {
-//        SmUsers user = userService.findOne(userId);
-//
-//        String existUser = "Y";
-//        if (user == null) {
-//            existUser = "N";
-//        }
-//        AtResponse<String> resp = new AtResponse(System.currentTimeMillis());
-//        resp.setResponseCode(ResponseCode.OK);
-//        resp.setData(existUser);
-//        return resp;
-//    }
-//
-//    //删除
-    @RequestMapping(value = "/delete")
-    @ResponseBody
-    public void delete(HttpServletResponse response, @RequestBody(required = true) String map) {
-        Map<String, String> parse = (Map) JSON.parse(map);
-        String userId = parse.get("userId");
-        smUsersRepService.delete(userId);
-        String message;
+
+    @ApiOperation(value = "查询用户唯一id是否存在", notes = "请求demo:{\"id\":\"213\"}",
+            httpMethod = "POST", responseContainer = "Map")
+    @ApiResponses({@ApiResponse(code = 200, message = "成功")})
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "map", value = "请求参数", required = true, paramType = "body")
+    })
+    @PostMapping(value = "/checkUserId")
+    public void checkUserId(HttpServletResponse response, @RequestBody String map) {
+        JSONObject object = JSON.parseObject(map);
+        String id = object.getString("id");
+        SmUsers user = smUsersRepService.findOne(id);
+        String existUser = "Y";
+        if (user == null) {
+            existUser = "N";
+        }
         AtResponse<String> resp = new AtResponse(System.currentTimeMillis());
-        message = "删除成功";
         resp.setResponseCode(ResponseCode.OK);
-        resp.setData(message);
+        resp.setData(existUser);
         wirte(response, resp);
     }
 
-    @RequestMapping(value = "/editor")
-    @ResponseBody
-    public AtResponse editor(HttpServletResponse response, @RequestBody SmUsers map) {
-        AtResponse<String> stringAtResponse = super.editSave(map, map.getUserId(), smUsersRepService);
-        return stringAtResponse;
+    @ApiOperation(value = "删除用户", notes = "请求demo:{\"id\":\"213\"}",
+            httpMethod = "POST", responseContainer = "Map")
+    @ApiResponses({@ApiResponse(code = 200, message = "成功")})
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "map", value = "请求参数", required = true, paramType = "body")
+    })
+    @RequestMapping(value = "/delete")
+    public void delete(HttpServletResponse response, @RequestBody String map) {
+        JSONObject object = JSON.parseObject(map);
+        String id = object.getString("id");
+        AtResponse<String> delete = super.delete(id, smUsersRepService);
+        wirte(response, delete);
     }
 
-    //修改密码
-    @RequestMapping(value = "/changePassword")
-    @ResponseBody
+    @ApiOperation(value = "查询编辑用户", notes = "请求demo:{\"id\":\"213\"}",
+            httpMethod = "POST", responseContainer = "Map")
+    @ApiResponses({@ApiResponse(code = 200, message = "成功")})
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "map", value = "请求参数", required = true, paramType = "body")
+    })
+    @RequestMapping(value = "/view")
+    public void view(HttpServletResponse response, @RequestBody String map) {
+        JSONObject object = JSON.parseObject(map);
+        String id = object.getString("id");
+        AtResponse<String> stringAtResponse = super.viewData(id, smUsersRepService);
+        wirte(response, stringAtResponse);
+    }
+
+    @ApiOperation(value = "编辑用户", notes = "请求demo:user实体类",
+            httpMethod = "POST", responseContainer = "Map")
+    @ApiResponses({@ApiResponse(code = 200, message = "成功")})
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "map", value = "请求参数", required = true, paramType = "body")
+    })
+    @RequestMapping(value = "/editor")
+    public void editor(HttpServletResponse response, @RequestBody SmUsers user) {
+        AtResponse resp = new AtResponse(System.currentTimeMillis());
+        if (StringUtils.isEmpty(user.getRoleId())) {
+            resp.setResponseCode(ResponseCode.INERERROR);
+            resp.setMessage("请选择角色！");
+        }
+        SmUsers updateUser = smUserService.editor(user, user.getRoleId());
+        if (updateUser == null) {
+            resp.setResponseCode(ResponseCode.INERERROR);
+            resp.setMessage("编辑失败,请重新添加！");
+        } else {
+            resp.setResponseCode(ResponseCode.OK);
+            resp.setMessage("编辑成功！");
+        }
+        wirte(response, resp);
+    }
+
+    @ApiOperation(value = "修改用户密码", notes = "请求demo:{\"id\":\"213\",\"jiuPassword\":\"12345\",\"password\":\"123\"} ",
+            httpMethod = "POST", responseContainer = "Map")
+    @ApiResponses({@ApiResponse(code = 200, message = "成功")})
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "map", value = "请求参数", required = true, paramType = "body")
+    })
+    @PostMapping(value = "/changePassword")
     public void updPwd(HttpServletResponse response, @RequestBody String map) {
         AtResponse<String> resp = new AtResponse(System.currentTimeMillis());
-        Map<String, String> parse = (Map) JSON.parse(map);
-        SmUsers user = smUsersRepService.findOne(getUserId());
-//        if (user.getUserPwd().equals(encoder.encodePassword(jiuPassword, null))) {
-        if (user.getUserPwd().equals(parse.get("jiuPassword"))) {
-//            String pwd = encoder.encodePassword(password, null);
-            smUsersRepService.setPasswordFor(parse.get("password"), getUserId());
+        JSONObject object = JSON.parseObject(map);
+        String userId = object.getString("id");
+        String jiuPassword = object.getString("jiuPassword");
+        String password = object.getString("password");
+        SmUsers user = smUsersRepService.findOne(userId);
+        if (user.getUserPwd().equals(jiuPassword)) {
+            smUsersRepService.setPasswordFor(password, userId);
             resp.setResponseCode(ResponseCode.OK);
             resp.setMessage("修改成功！");
         } else {
             resp.setResponseCode(ResponseCode.INERERROR2);
             resp.setMessage("原密码错误！");
         }
-
         wirte(response, resp);
-
-
-    }
-
-    //保存编辑
-    @PostMapping(value = "/editorUser")
-    @ResponseBody
-    public AtResponse editorUser(HttpServletResponse response, @RequestBody SmUsers user) {
-
-        AtResponse<String> resp = new AtResponse(System.currentTimeMillis());
-        if (StringUtils.isEmpty(user.getRoleId())) {
-            resp.setResponseCode(ResponseCode.INERERROR);
-            resp.setMessage("请选择角色！");
-            return resp;
-        }
-        SmUsers updateUser = smUserService.editor(user, user.getRoleId());
-        if (updateUser == null) {
-            resp.setResponseCode(ResponseCode.INERERROR);
-            resp.setMessage("编辑失败,请重新添加！");
-            return resp;
-        } else {
-            resp.setResponseCode(ResponseCode.OK);
-            resp.setMessage("编辑成功！");
-            return resp;
-        }
-
     }
 }
