49324a08f4b36c77bc7e40ec059ca2273ac4456d
zhouziyu <1107536493@qq.com>
2019-01-25 10:03:14 +0800

    测试demo

--- cloud-page/src/main/java/com/jhmk/cloudpage/controller/DemoController.java
+++ cloud-page/src/main/java/com/jhmk/cloudpage/controller/DemoController.java
@@ -113,7 +113,8 @@ public class DemoController extends BaseController {
 //2、获取输出流，向服务器端发送信息
         OutputStream os = socket.getOutputStream();//字节输出流
         PrintWriter pw = new PrintWriter(os);//将输出流包装成打印流
-        pw.write("0000969000#1###binganshouye");
+        pw.write("0000969000#1###ruyuanjilu");
+//        pw.write("0000969000#1###binganshouye");
         pw.flush();
         socket.shutdownOutput();
 //3、获取输入流，并读取服务器端的响应信息
@@ -142,11 +143,11 @@ public class DemoController extends BaseController {
     @GetMapping("/getRule")
     public void getRule(HttpServletResponse response) {
         JiaheRuleBean jiaheRuleBean = new JiaheRuleBean();
-        Binganshouye binganshouyeFromETL = binganshouyeService.getBinganshouyeFromETL("0000969000", "1");
+        Binganshouye binganshouyeFromETL = binganshouyeService.getBinganshouyeFromETL("0000969000", "1",socketPropConf.getIp(),socketPropConf.getPort2());
         jiaheRuleBean.setBinganshouye(binganshouyeFromETL);
-        List<Jianyanbaogao> jianyanbaogao = jianyanbaogaoService.getJianyanbaogao(jiaheRuleBean);
+        List<Jianyanbaogao> jianyanbaogao = jianyanbaogaoService.getJianyanbaogao(jiaheRuleBean,socketPropConf.getIp(),socketPropConf.getPort());
         List<Jianyanbaogao> distinctJianyanbaogao = jianyanbaogaoService.getDistinctJianyanbaogao(jianyanbaogao);
-        List<Jianchabaogao> jianchabaogao = jianchabaogaoService.getJianchabaogao(jiaheRuleBean);
+        List<Jianchabaogao> jianchabaogao = jianchabaogaoService.getJianchabaogao(jiaheRuleBean,socketPropConf.getIp(),socketPropConf.getPort());
         wirte(response, "123");
     }
 }
